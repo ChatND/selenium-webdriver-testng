1a84be70185f99ed1c52940e7f07074db27d310e
ADMIN <ADMIN@ChatND>
2022-12-28 22:07:21 +0700

    update Topic 01

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -13,11 +13,16 @@ import org.testng.annotations.Test;
 public class Topic_01_Check_Environment {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
+	String osName = System.getProperty("os.name");
 
 	@BeforeClass
 	public void beforeClass() {
-		System.setProperty("webdriver.gecko.driver", "D:\\Online_Class_25\\02 - Selenium WebDriver\\browserDriver\\geckodriver.exe");
-		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		if (osName.contains("Windows")) {
+			System.setProperty("webdriver.gecko.driver", "D:\\Online_Class_25\\02 - Selenium WebDriver\\browserDriver\\geckodriver.exe");
+		} else {
+			System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+		}
+
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
@@ -25,21 +30,22 @@ public class Topic_01_Check_Environment {
 	}
 
 	@Test
-	public void TC_01() {
+	public void TC_01_ValidateCurrentUrl() {
 		// Login Page Url matching
 		String loginPageUrl = driver.getCurrentUrl();
 		Assert.assertEquals(loginPageUrl, "https://www.facebook.com/");
+
 	}
 
 	@Test
-	public void TC_02() {
+	public void TC_02_ValidatePageTitle() {
 		// Login Page title
 		String loginPageTitle = driver.getTitle();
 		Assert.assertEquals(loginPageTitle, "Facebook – log in or sign up");
 	}
 
 	@Test
-	public void TC_03() {
+	public void TC_03_LoginFormDisplayed() {
 		// Login form displayed
 		Assert.assertTrue(driver.findElement(By.xpath("//form[@data-testid='royal_login_form']")).isDisplayed());
 	}
